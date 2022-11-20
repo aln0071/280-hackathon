ef462585a97dbc2fcb73ae8220303a7eae04c71a
Snichat97 <34672064+Snichat97@users.noreply.github.com>
2022-11-20 00:44:37 -0800

    Changing heading.

--- frontend/src/components/header/index.jsx
+++ frontend/src/components/header/index.jsx
@@ -4,7 +4,7 @@ import logo from '../../logo.svg';
 export default function Header() {
     return <div className="header">
         <img alt="logo" src={logo} width={80} />
-        <h3 className="page-header">heading</h3>
+        <h3 className="page-header">Food Security Time Series Dashboard</h3>
         <div className="country-flag">country flag</div>
     </div>
 }
